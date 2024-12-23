a1fb192be353b6754bb0f5d0cfc2a866b1d0da43
vladimir <vladimirkozikjob@gmail.com>
2024-12-23 20:51:22 -0300

    Lista la conexion con firebase y añadi ignore

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-document.getElementById('formulario').addEventListener('submit',(event) =>{
+// Importa los módulos necesarios desde Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDUimiHowyfAitQaDsJvaf_wlDaHfedyE4",
+    authDomain: "form-d270a.firebaseapp.com",
+    projectId: "form-d270a",
+    storageBucket: "form-d270a.appspot.com",
+    messagingSenderId: "410862454122",
+    appId: "1:410862454122:web:93ffd46f7127454b269113",
+    measurementId: "G-F05FCHGLYF"
+};
+
+// Inicializar Firebase y Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById('formulario').addEventListener('submit',async (event) =>{
     event.preventDefault()
 
     //Validad campo nombre
@@ -27,7 +46,8 @@ document.getElementById('formulario').addEventListener('submit',(event) =>{
     //Validad contraseña
     let contrasenaEntrada = document.getElementById('password')
     let contrasenaError = document.getElementById('passwordError')
-    if(contrasenaEntrada.value.length < 8){
+    let contrasenaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+    if(!contrasenaPattern.test(contrasenaEntrada.value)){
         contrasenaError.textContent = 'La contraseña debe tener al menos 8 caracteres'
         contrasenaError.classList.add('error-message')
     }else{
@@ -37,7 +57,17 @@ document.getElementById('formulario').addEventListener('submit',(event) =>{
     //Si todos los campos son válidos enviar form
     if(!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent){
         //Backend que reciba la info
-        alert('El formulario se ha enviado con exito')
-        document.getElementById('formulario').reset()
+        try {
+            // Agregar un documento a la colección "users"
+            await addDoc(collection(db, "users"), {
+                nombre: entradaNombre.value,
+                email: emailEntrada.value,
+                password: contrasenaEntrada.value
+            });
+            alert('El formulario se ha enviado con éxito');
+            document.getElementById('formulario').reset();
+        } catch (error) {
+            console.error("Error al guardar los datos:", error);
+        }
     }
 })
